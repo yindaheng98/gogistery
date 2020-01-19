247eea0ab86e3b1201976fafc8556bb74391c220
yindaheng98 <yindaheng98@163.com>
2020-01-19 22:46:43 +0800

    Info小改

--- base/ReceiverInfo.go
+++ base/ReceiverInfo.go
@@ -5,4 +5,5 @@ import "time"
 type ReceiverInfo interface {
 	GetAddr() string           //获取下一次的请求地址
 	GetTimeout() time.Duration //获取下一次请求的间隔时间
+	GetRetryN() uint32         //获取最大重试次数
 }
--- base/SenderInfo.go
+++ base/SenderInfo.go
@@ -1,5 +1,8 @@
 package base
 
+import "time"
+
 type SenderInfo interface {
-	Send(addr string) (ReceiverInfo, error)
+	Send(addr string, timeout time.Duration) (ReceiverInfo, error)
+	//这个里面的timeout项用于指定超时时间，超过此时间即判定为发送失败
 }
