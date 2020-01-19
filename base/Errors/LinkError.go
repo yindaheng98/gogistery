32e58a2ed28282ce4d5f4aad29c0e0dff90af56a
yindaheng98 <yindaheng98@163.com>
2020-01-19 22:49:03 +0800

    用Link重写了Error

---
+++ base/Errors/LinkError.go
@@ -0,0 +1,17 @@
+package Errors
+
+import "gogistery/base"
+
+//用于指示在“从哪个sender到哪个receiver的发送过程中发生了什么error”
+type LinkError struct {
+	error
+	link base.LinkInfo
+}
+
+func NewLinkError(err error, pairInfo base.LinkInfo) LinkError {
+	return LinkError{error: err, link: pairInfo}
+}
+
+func (e *LinkError) Pair() base.LinkInfo {
+	return e.link
+}
