3537b2766a0401a79ff3b592d10aeeeab16e426f
yindaheng98 <yindaheng98@163.com>
2020-01-23 15:05:58 +0800

    使用单线程循环操作类改造了触发器

--- util/Emitter/Emitter.go
+++ util/Emitter/Emitter.go
@@ -1,24 +1,29 @@
 package Emitter
 
 import (
+	"gogistery/util/Single"
 	"sync"
-	"sync/atomic"
 )
 
 //线程安全的触发器类，多线程输入事件->单线程处理事件
 type Emitter struct {
-	enabled    uint32              //触发器状态（是否启动）
+	runner     *Single.Processor   //控制事件处理线程
 	handlers   []func(interface{}) //事件处理器列表
 	handlersMu *sync.RWMutex       //事件处理器列表读写锁
 	events     chan interface{}    //事件队列
+	eventsMu   *sync.RWMutex       //事件队列的新建删除和使用操作锁
 }
 
 //新建触发器
 func New() *Emitter {
-	return &Emitter{0,
+
+	r := &Emitter{nil,
 		[]func(interface{}){},
 		new(sync.RWMutex),
-		make(chan interface{})}
+		make(chan interface{}),
+		new(sync.RWMutex)}
+	r.runner = Single.NewProcessor(r.eventLoop)
+	return r
 }
 
 //添加一个事件处理函数
@@ -35,34 +40,25 @@ func (e *Emitter) Emit(info interface{}) {
 			e.Disable()
 		}
 	}()
-	if atomic.LoadUint32(&e.enabled) != 0 { //只有不在disabled状态才入队列
+	e.eventsMu.RLock()
+	defer e.eventsMu.RUnlock()
+	if e.runner.IsRunning() { //只有不在disabled状态才入队列
 		e.events <- info
 	}
 }
 
 //启动事件循环
 func (e *Emitter) Enable() {
-	if atomic.CompareAndSwapUint32(&e.enabled, 0, 1) { //处于disabled状态才启动
-		go e.routine() //启动事件处理循环
-	}
+	e.runner.Start()
 }
 
 //停止事件循环
 func (e *Emitter) Disable() {
-	if atomic.CompareAndSwapUint32(&e.enabled, 1, 0) { //处于enabled状态才进行停止操作
-		close(e.events)
-		e.events = make(chan interface{})
-	}
-}
-
-//goroutine循环调用事件处理函数
-func (e *Emitter) routine() {
-	for {
-		if atomic.CompareAndSwapUint32(&e.enabled, 0, 0) { //如果要停止
-			break //那就停止
-		}
-		e.eventLoop() //事件处理循环
-	}
+	e.runner.Stop()
+	e.eventsMu.Lock()
+	defer e.eventsMu.Unlock()
+	close(e.events)
+	e.events = make(chan interface{})
 }
 
 //事件处理循环：出队列处理事件
