d1dd0aefbec6b6d4231b24b662455b95449fe5c3
yindaheng98 <yindaheng98@163.com>
2020-01-31 13:06:17 +0800

    注释补全

--- base/ReceiverInfo.go
+++ base/ReceiverInfo.go
@@ -2,6 +2,7 @@ package base
 
 import "time"
 
+//此接口仅负责记录在发送和接收器之间传递的消息
 type ReceiverInfo interface {
 	GetAddr() string           //获取下一次的请求地址
 	GetTimeout() time.Duration //获取下一次请求的间隔时间
--- base/SenderInfo.go
+++ base/SenderInfo.go
@@ -1,5 +1,6 @@
 package base
 
+//此接口仅负责记录在发送和接收器之间传递的消息
 type SenderInfo interface {
 	GetID() string      //获取ID
 	IsDisconnect() bool //是否请求中断连接
