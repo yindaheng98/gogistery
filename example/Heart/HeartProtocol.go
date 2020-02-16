5f40c702cf616716c61c4e62664911357b256771
yindaheng98 <yindaheng98@163.com>
2020-02-16 22:01:08 +0800

    基于ChanNet虚拟网络构建的Heart例程测试完成

--- example/Heart/HeartProtocol.go
+++ example/Heart/HeartProtocol.go
@@ -3,40 +3,62 @@ package Heart
 import (
 	"fmt"
 	"gogistery/Protocol"
+	ExampleProtocol "gogistery/example/Protocol"
 	"time"
 )
 
 type RequesterHeartProtocol struct {
-	lastReq     Protocol.TobeSendRequest
-	lastTimeout time.Duration
-	lastRetryN  uint64
-	n           uint64
+	Info ExampleProtocol.RegistrantInfo
+	n    int64
 }
 
+func NewRequesterHeartProtocol(info ExampleProtocol.RegistrantInfo, BeatN int64) *RequesterHeartProtocol {
+	return &RequesterHeartProtocol{Info: info, n: BeatN}
+}
 func (r *RequesterHeartProtocol) Beat(response Protocol.Response, beat func(Protocol.TobeSendRequest, time.Duration, uint64)) {
-	s := "\n------RequesterHeartProtocol.Beat------>"
-	request := r.lastReq
+	s := "------RequesterHeartProtocol.Beat------>"
+	defer func() { fmt.Print(s + "\n") }()
 	s += fmt.Sprintf("No.%d beat was success with a response %s. ", r.n, response.String())
-	r.n++
-	if r.n < 10 {
-		fmt.Print(s + fmt.Sprintf("And the next beat is %s. ", request.String()))
-		beat(request, r.lastTimeout, r.lastRetryN)
-	} else {
-		fmt.Print(s + "And it's the end of beating.")
+	if r.n--; r.n < 0 {
+		s += "And it's the end of beating."
+		return
 	}
+	request := Protocol.TobeSendRequest{
+		Request: Protocol.Request{
+			RegistrantInfo: r.Info,
+			Disconnect:     false,
+		},
+		Option: response.RegistryInfo.GetRequestSendOption(),
+	}
+	s += fmt.Sprintf("And the next beat is %s. ", request.String())
+	beat(request, response.Timeout, response.RetryN)
 }
 
 type ResponserHeartProtocol struct {
-	lastRes Protocol.TobeSendResponse
+	Info    ExampleProtocol.RegistryInfo
+	Timeout time.Duration
+	RetryN  uint64
 	n       uint64
 }
 
+func NewResponserHeartProtocol(info ExampleProtocol.RegistryInfo, Timeout time.Duration, RetryN uint64) *ResponserHeartProtocol {
+	return &ResponserHeartProtocol{Info: info, Timeout: Timeout, RetryN: RetryN, n: 0}
+}
+
 func (r *ResponserHeartProtocol) Beat(request Protocol.Request) Protocol.TobeSendResponse {
-	s := "\n------ResponserHeartProtocol.Beat------>"
+	s := "------ResponserHeartProtocol.Beat------>"
+	defer func() { fmt.Print(s + "\n") }()
 	s += fmt.Sprintf("No.%d request %s arrived. ", r.n, request.String())
 	r.n++
-	res := r.lastRes
-	fmt.Print(s + fmt.Sprintf("And the response will be %s with option %s. ",
-		res.Response.String(), res.Option.String()))
-	return res
+	response := Protocol.TobeSendResponse{
+		Response: Protocol.Response{
+			RegistryInfo: r.Info,
+			Timeout:      r.Timeout,
+			RetryN:       r.RetryN,
+			Reject:       false,
+		},
+		Option: request.RegistrantInfo.GetResponseSendOption(),
+	}
+	s += fmt.Sprintf("And the response will be %s. ", response.String())
+	return response
 }
--- example/Heart/Heart_test.go
+++ example/Heart/Heart_test.go
@@ -1,41 +1,77 @@
 package Heart
 
 import (
+	"fmt"
 	"gogistery/Heart"
 	"gogistery/Protocol"
+	ExampleProtocol "gogistery/example/Protocol"
+	"math/rand"
 	"testing"
 	"time"
 )
 
-func TestRequesterHeart(t *testing.T) {
-	requester := Heart.NewRequesterHeart(&RequesterHeartProtocol{
-		lastReq: Protocol.TobeSendRequest{
-			Request: Request{ID: "0"},
-			Option:  RequestSendOption{ID: "0", Addr: "0.0.0.0"}},
-		lastTimeout: 10e9,
-		lastRetryN:  10,
-		n:           0},
-		NewRequestBeatProtocol())
-	err := requester.RunBeating(
-		Protocol.TobeSendRequest{
-			Request: Request{ID: "1"},
-			Option:  RequestSendOption{ID: "1", Addr: "1.1.1.1"}},
-		10e9, 10,
-	)
-	t.Log(err)
+func ChanNetRequesterHeartTest(t *testing.T, RegistrantID string, initAddr string) {
+	s := fmt.Sprintf("--ChanNetRequesterHeartTest(RegistrantID:%s,initAddr:%s)-->", RegistrantID, initAddr)
+	info := ExampleProtocol.RegistrantInfo{
+		ID:     RegistrantID,
+		Option: ExampleProtocol.ResponseSendOption{Timestamp: time.Now()},
+	}
+	requester := Heart.NewRequesterHeart(
+		NewRequesterHeartProtocol(info, 10),
+		ExampleProtocol.NewChanNetRequestProtocol())
+	requester.Events.Retry.AddHandler(func(o Protocol.TobeSendRequest, err error) {
+		t.Log(s + fmt.Sprintf("A request %s retryed because %s", o.String(), err.Error()))
+	})
+	requester.Events.Retry.Enable()
+	go func() {
+		t.Log(s + fmt.Sprintf("RequesterHeart started with info %s.", info.String()))
+		err := requester.RunBeating(Protocol.TobeSendRequest{
+			Request: Protocol.Request{RegistrantInfo: info, Disconnect: false},
+			Option:  ExampleProtocol.RequestSendOption{RequestAddr: initAddr, Timestamp: time.Now()},
+		}, 10e9, 10)
+		if err != nil {
+			t.Log(s+"RequesterHeart stopped with error: %s.", err.Error())
+		} else {
+			t.Log(s + "RequesterHeart stopped normally.")
+		}
+	}()
 }
 
-func TestResponserHeart(t *testing.T) {
-	responser := Heart.NewResponserHeart(&ResponserHeartProtocol{
-		lastRes: Protocol.TobeSendResponse{
-			Response: Response{ID: "0"},
-			Option:   ResponseSendOption{ID: "0"}},
-	}, NewResponseBeatProtocol("0"))
-	go responser.RunBeating()
-	time.Sleep(1e9)
-	responser.Stop()
-	responser.Stop()
-	go responser.RunBeating()
-	time.Sleep(1e9)
-	responser.Stop()
+func ChanNetResponserHeartTest(t *testing.T, RegistryID string) string {
+	s := fmt.Sprintf("--ChanNetRequesterHeartTest(RegistryID:%s)-->", RegistryID)
+	proto := ExampleProtocol.NewChanNetResponseProtocol()
+	info := ExampleProtocol.RegistryInfo{
+		ID:         RegistryID,
+		Option:     ExampleProtocol.RequestSendOption{RequestAddr: proto.GetAddr(), Timestamp: time.Now()},
+		Candidates: []Protocol.RegistryInfo{}}
+	responser := Heart.NewResponserHeart(NewResponserHeartProtocol(info, 2e9, 5), proto)
+	responser.Event.Error.AddHandler(func(err error) {
+		t.Log(s + fmt.Sprintf("An error occurred: %s", err.Error()))
+	})
+	responser.Event.Error.Enable()
+	go func() {
+		t.Log(s + fmt.Sprintf("ResponserHeart started with info %s.", info.String()))
+		go responser.RunBeating()
+		time.Sleep(5e9)
+		responser.Stop()
+		responser.Stop()
+		go responser.RunBeating()
+		time.Sleep(1e9)
+		responser.Stop()
+	}()
+	return proto.GetAddr()
+}
+
+const REQUESTERN = 30
+const RESPONSERN = 10
+
+func TestChanNetHeart(t *testing.T) {
+	responsers := make([]string, RESPONSERN)
+	for i := 0; i < RESPONSERN; i++ {
+		responsers[i] = ChanNetResponserHeartTest(t, fmt.Sprintf("RESPONSER_%02d", i))
+	}
+	for i := 0; i < REQUESTERN; i++ {
+		ChanNetRequesterHeartTest(t, fmt.Sprintf("REQUESTER_%02d", i), responsers[rand.Intn(RESPONSERN)])
+	}
+	time.Sleep(8e9)
 }
