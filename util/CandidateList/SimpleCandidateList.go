196f02075ec585094d7a1b4d52a9007ff2459fc6
yindaheng98 <yindaheng98@163.com>
2020-02-23 17:23:02 +0800

    优化逻辑

--- util/CandidateList/SimpleCandidateList.go
+++ util/CandidateList/SimpleCandidateList.go
@@ -3,61 +3,61 @@ package CandidateList
 import (
 	"github.com/yindaheng98/go-utility/SortedSet"
 	"gogistery/protocol"
-	"sync"
 	"time"
 )
 
 type SimpleCandidateList struct {
 	initTimeout time.Duration
 	initRetryN  uint64
-	set         *SortedSet.SortedSet
-	setMu       *sync.RWMutex
-	waitChan    chan bool
+	set         chan *SortedSet.SortedSet
+	waitGroup   chan bool
 }
 
 func NewSimpleCandidateList(size uint64, initRegistry protocol.RegistryInfo, initTimeout time.Duration, initRetryN uint64) *SimpleCandidateList {
-	proto := &SimpleCandidateList{
+	list := &SimpleCandidateList{
 		initTimeout: initTimeout,
 		initRetryN:  initRetryN,
-		set:         SortedSet.New(size),
-		setMu:       new(sync.RWMutex),
-		waitChan:    make(chan bool, 1),
+		set:         make(chan *SortedSet.SortedSet, 1),
+		waitGroup:   make(chan bool),
 	}
-	proto.set.Update(element{initRegistry}, 0)
-	return proto
+	set := SortedSet.New(size)
+	set.Update(element{initRegistry}, 0)
+	list.set <- set
+	return list
 }
 
-func (p *SimpleCandidateList) StoreCandidates(response protocol.Response) {
-	p.setMu.Lock()
-	defer p.setMu.Unlock()
-	p.set.DeltaUpdateAll(-1)
+func (list *SimpleCandidateList) StoreCandidates(response protocol.Response) {
+	set := <-list.set
+	defer func() {
+		list.set <- set                  //完成后放回队列
+		close(list.waitGroup)            //唤醒所有等待的获取队列进程
+		list.waitGroup = make(chan bool) //然后再阻塞之
+	}()
+	set.DeltaUpdateAll(-1) //先让所有元素优先级下降1
 	candidates := response.RegistryInfo.GetCandidates()
 	for _, candidate := range candidates {
 		el := element{candidate}
-		if _, exists := p.set.GetWeight(el); exists {
-			p.set.DeltaUpdate(el, 1)
+		if _, exists := set.GetWeight(el); exists {
+			set.DeltaUpdate(el, 1) //然后让响应中给出的元素优先级上升1
 		} else {
-			p.set.Update(el, 0)
+			set.Update(el, 0) //或者加入新元素
 		}
 	}
-	close(p.waitChan)               //唤醒所有等待进程
-	p.waitChan = make(chan bool, 1) //然后再阻塞之
 }
 
-func (p *SimpleCandidateList) GetCandidate(excepts []protocol.RegistryInfo) (protocol.RegistryInfo, time.Duration, uint64) {
+func (list *SimpleCandidateList) GetCandidate(excepts []protocol.RegistryInfo) (protocol.RegistryInfo, time.Duration, uint64) {
 	for {
-		p.setMu.Lock()
+		set := <-list.set
 		for _, except := range excepts {
-			p.set.Remove(element{except})
+			set.Remove(element{except}) //先移除所有除外元素
 		}
-
-		if els := p.set.Sorted(1); len(els) > 0 {
-			el := els[0].(element)
-			p.set.Remove(el)
-			p.setMu.Unlock()
-			return el.RegistryInfo, p.initTimeout, p.initRetryN
+		if els := set.Sorted(1); len(els) > 0 { //然后看是否还有剩余元素
+			el := els[0].(element) //有就作为返回结果
+			set.Remove(el)         //然后删除返回结果
+			list.set <- set        //并将集合放回队列
+			return el.RegistryInfo, list.initTimeout, list.initRetryN
 		}
-		p.setMu.Unlock()
-		<-p.waitChan
+		list.set <- set  //并将集合放回队列
+		<-list.waitGroup //等待更新
 	}
 }
