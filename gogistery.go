46fdba989fbdabf21986269cda3117d28e9b2b59
yindaheng98 <yindaheng98@163.com>
2020-02-24 13:42:37 +0800

    项目大致完成

--- gogistery.go
+++ gogistery.go
@@ -1,1 +1,24 @@
 package gogistery
+
+import (
+	"gogistery/protocol"
+	"gogistery/registrant"
+	"gogistery/registry"
+)
+
+func NewRegistry(
+	Info protocol.RegistryInfo,
+	maxRegistrants uint,
+	timeoutController registry.TimeoutController,
+	ResponseProto protocol.ResponseProtocol) *registry.Registry {
+	return registry.New(Info, maxRegistrants, timeoutController, ResponseProto)
+}
+
+func NewRegistrant(
+	Info protocol.RegistrantInfo,
+	regitryN uint,
+	CandidateList registrant.RegistryCandidateList,
+	retryNController registrant.RetryNController,
+	RequestProto protocol.RequestProtocol) *registrant.Registrant {
+	return registrant.New(Info, regitryN, CandidateList, retryNController, RequestProto)
+}
--- gogistery_test.go
+++ gogistery_test.go
@@ -1,28 +1,99 @@
 package gogistery
 
 import (
-	"gogistery/proto"
+	"fmt"
+	ExampleProtocol "gogistery/example/protocol"
+	"gogistery/protocol"
+	"gogistery/util/CandidateList"
+	"gogistery/util/RetryNController"
+	"gogistery/util/TimeoutController"
 	"testing"
+	"time"
 )
 
-func TestServerInfo(t *testing.T) {
-	t.Log("服务器和客户端信息测试")
-	sInfo := proto.ServerInfo{
-		ServiceType:    "testService",
-		ID:             "test",
-		Addr:           "test.test.wxstc",
-		RelatedServers: []string{"a.test.o"},
-	}
-	sInfoB, err := sInfo.String()
-	if err != nil {
-		t.Error(err)
-	}
-	sInfoS := string(sInfoB)
-	t.Log(sInfoS)
+var RegistryInfos = make(map[string]ExampleProtocol.RegistryInfo)
+var LastRegistryInfo protocol.RegistryInfo
 
-	sInfoP, err := proto.ParseServer([]byte(sInfoS))
-	if err != nil {
-		t.Error(err)
+func RegistryTest(t *testing.T) {
+	proto := ExampleProtocol.NewChanNetResponseProtocol()
+	info := ExampleProtocol.RegistryInfo{
+		ID: "REGISTRY_" + proto.GetAddr(),
+		Option: ExampleProtocol.RequestSendOption{
+			RequestAddr: proto.GetAddr(),
+			Timestamp:   time.Now(),
+		},
+		Candidates: nil,
 	}
-	t.Log(sInfoP)
+	for _, RegistryInfo := range RegistryInfos {
+		info.Candidates = append(info.Candidates, RegistryInfo)
+	}
+	RegistryInfos[proto.GetAddr()] = info
+	LastRegistryInfo = info
+	r := NewRegistry(info, 5,
+		TimeoutController.NewLogTimeoutController(1e9, 3e9, 2),
+		proto)
+	r.Events.NewConnection.AddHandler(func(i protocol.RegistrantInfo) {
+		t.Log(fmt.Sprintf("RegistryTest:%s--NewConnection--%s", info.GetRegistryID(), i.GetRegistrantID()))
+	})
+	r.Events.NewConnection.Enable()
+	r.Events.Disconnection.AddHandler(func(i protocol.RegistrantInfo) {
+		t.Log(fmt.Sprintf("RegistryTest:%s--Disconnection--%s", info.GetRegistryID(), i.GetRegistrantID()))
+	})
+	r.Events.Disconnection.Enable()
+	go func() {
+		r.Run()
+		fmt.Printf("%s stopped itself.\n", info.ID)
+	}()
+	go func() {
+		time.Sleep(15e9)
+		r.Stop()
+		t.Log(fmt.Sprintf("%s stopped manually.", info.ID))
+	}()
+}
+
+const SERVERN = 5
+const CLIENTN = 30
+
+func RegistrantTest(t *testing.T, i int) {
+	proto := ExampleProtocol.NewChanNetRequestProtocol()
+	info := ExampleProtocol.RegistrantInfo{
+		ID:     fmt.Sprintf("REGISTRANT_%02d", i),
+		Option: ExampleProtocol.ResponseSendOption{},
+	}
+	r := NewRegistrant(info, 5,
+		CandidateList.NewSimpleCandidateList(SERVERN, LastRegistryInfo, 2e9, 10),
+		RetryNController.SimpleRetryNController{}, proto)
+	r.Events.NewConnection.AddHandler(func(i protocol.RegistryInfo) {
+		t.Log(fmt.Sprintf("RegistrantTest:%s--NewConnection--%s", info.GetRegistrantID(), i.GetRegistryID()))
+	})
+	r.Events.NewConnection.Enable()
+	r.Events.Disconnection.AddHandler(func(request protocol.TobeSendRequest, err error) {
+		t.Log(fmt.Sprintf("RegistrantTest:%s--Disconnection--%s. error:%s",
+			info.GetRegistrantID(), request.Option.String(), err))
+	})
+	r.Events.Disconnection.Enable()
+	r.Events.Error.AddHandler(func(err error) {
+		t.Log(fmt.Sprintf("RegistrantTest:%s--Error--%s", info.GetRegistrantID(), err))
+	})
+	r.Events.Error.Enable()
+	go func() {
+		r.Run()
+		fmt.Printf("%s stopped itself.\n", info.ID)
+	}()
+	go func() {
+		time.Sleep(10e9)
+		r.Stop()
+		t.Log(fmt.Sprintf("%s stopped manually.", info.ID))
+	}()
+}
+
+func TestRegistryRegistrant(t *testing.T) {
+	for i := 0; i < SERVERN; i++ {
+		RegistryTest(t)
+	}
+	time.Sleep(1e9)
+	for i := 0; i < CLIENTN; i++ {
+		RegistrantTest(t, i)
+	}
+	time.Sleep(20e9)
 }
