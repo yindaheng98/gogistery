f94dd8dff0bc27a57c95b860dc8ac1885e6ac9a3
yindaheng98 <yindaheng98@163.com>
2020-03-04 19:49:10 +0800

    小改

--- registry/beater.go
+++ registry/beater.go
@@ -13,22 +13,22 @@ func (p *beater) Beat(request protocol.Request) protocol.TobeSendResponse {
 		return protocol.TobeSendResponse{
 			Response: protocol.Response{
 				RegistryInfo: p.registry.Info,
-				Timeout:      0,
+				Timeout:      3600 * 24 * 1e9,
 				Reject:       true}, //拒绝连接
 			Option: request.RegistrantInfo.GetResponseSendOption()}
-	} else if timeout, ok := p.registry.register(request); ok {
+	} else if timeout, ok := p.registry.register(request); !ok { //注册中心表示不能连接
 		return protocol.TobeSendResponse{
 			Response: protocol.Response{
 				RegistryInfo: p.registry.Info,
 				Timeout:      timeout,
-				Reject:       false}, //同意连接
+				Reject:       true}, //拒绝连接
 			Option: request.RegistrantInfo.GetResponseSendOption()}
 	} else {
 		return protocol.TobeSendResponse{
 			Response: protocol.Response{
 				RegistryInfo: p.registry.Info,
 				Timeout:      timeout,
-				Reject:       true}, //拒绝连接
+				Reject:       false}, //同意连接
 			Option: request.RegistrantInfo.GetResponseSendOption()}
 	}
 }
