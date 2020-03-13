f9d3aeb4d3d08e103dcaf28c4699999beaf477c2
yindaheng98 <yindaheng98@163.com>
2020-03-13 22:03:13 +0800

    godoc updated

--- protocol/doc.go
+++ protocol/doc.go
@@ -1,5 +1,5 @@
 /*
-This package is the basic of the gogistry.
+Package protocol is the basic of the gogistry.
 Before using gogistry, you should implement the interfaces using the transmission protocol you like (i.e. http, grpc, etc) in this package first.
 */
 package protocol
