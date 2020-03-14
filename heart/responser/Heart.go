cc4042e459d60b1fe0d70c31981fa707872a115a
yindaheng98 <yindaheng98@163.com>
2020-03-14 13:06:43 +0800

    godoc updated

--- heart/responser/Heart.go
+++ heart/responser/Heart.go
@@ -5,19 +5,35 @@ import (
 	"github.com/yindaheng98/gogistry/protocol"
 )
 
+//Heart can receive the requests and send back response in a loop,
+//until stopped by context.
 type Heart struct {
-	beater       HeartBeater
-	responser    *responser
+	beater    HeartBeater
+	responser *responser
+
+	//This function will be called when an error occurred.
 	ErrorHandler func(error)
 }
 
+//NewHeart returns the pointer to a heart.
 func NewHeart(beater HeartBeater, ResponseProto protocol.ResponseProtocol) *Heart {
 	return &Heart{beater,
 		newResponser(ResponseProto),
 		func(error) {}}
 }
 
-//开始接收心跳，直到主动停止
+//RunBeating is a loop to receive the requests and send back response.
+//It will do the following:
+//
+//1. Call the method `protocol.ResponseProtocol.Response` you have implemented to receive a request and a function for sending back response
+//
+//2. Call the method `HeartBeater.Beat` you have implemented to get a response
+//
+//3. Send back the response using the received sending-back function
+//
+//4. Loop step 1~3 until a stop signal arrived
+//
+//When called, this function will block your goroutine until returned.
 func (h *Heart) RunBeating(ctx context.Context) {
 	for {
 		var request protocol.Request
--- heart/responser/HeartBeater.go
+++ heart/responser/HeartBeater.go
@@ -5,9 +5,18 @@ import (
 	"github.com/yindaheng98/gogistry/protocol"
 )
 
+//HeartBeater is the controller of the Heart.
+//It is used in Heart to generate response.
 type HeartBeater interface {
-	//对接上层消息策略，每一个成功到达的数据请求都必须有响应
+	//Beat was designed to generate a response (`protocol.Response` and `protocol.ResponseSendOption` in `protocol.TobeSendResponse`) from a received request (`Protocol.Request`).
+	//In the implementaion of this method, you should do the following:
 	//
-	//输入一个Beat数据请求，处理请求并生成Beat数据响应
+	//1. Cast the `request` into your implementaion type
+	//
+	//2. Generate a `protocol.Response` you want to send back to the request and the send option `protocol.ResponseSendOption`
+	//
+	//3. Enclose the `protocol.Response` and `protocol.ResponseSendOption` into a `protocol.TobeSendResponse`
+	//
+	//4. Return the `protocol.TobeSendResponse`
 	Beat(ctx context.Context, request protocol.Request) protocol.TobeSendResponse
 }
